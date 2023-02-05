561c47cb2c9d01fb861dc3d5e521a2506f502639
Florencia Bernachia <florenciabernachia@gmail.com>
2023-02-05 23:38:42 +0100

    detalles css

---
+++ js/carrito.js
@@ -0,0 +1,111 @@
+const productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+console.log(productosEnCarrito);
+const carritoVacio = document.querySelector ("#carrito-vacio");
+const contenedorCompra = document.querySelector ("#comprado");
+const carritoProductos = document.querySelector ("#carrito-productos");
+const carritoAcciones = document.querySelector ("#carrito-acciones");
+const carritoVaciar = document.querySelector (".carrito-vaciar")
+const botonComprar = document.querySelector ("#carrito-acciones-comprar");
+let botonesEliminar = document.querySelectorAll (".carrito-eliminar-producto")
+const total = document.querySelector (".total");
+
+
+
+function cargarProductosCarrito (){
+
+    if (productosEnCarrito && productosEnCarrito.length > 0 ){
+        carritoVacio.classList.add("disabled");
+        carritoProductos.classList.remove("disabled");
+        carritoAcciones.classList.remove("disabled");
+        carritoVaciar.classList.remove("disabled");
+        contenedorCompra.classList.add("disabled");
+    
+        carritoProductos.innerHTML = "";
+    
+        productosEnCarrito.forEach(producto => {
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+    
+            <img class="carrito-imagen-producto" src="../assets/imagenes/${producto.img}" alt="">
+            <div class="carrito-titulo-producto">
+                <small>Producto</small>
+                <h3>${producto.nombre}</h3>
+            </div>
+            <div class="carrito-cantidad-producto">
+                <small>Cantidad</small>
+                <p>${producto.cantidad}</p>
+            </div>
+            <div class="carrito-precio-producto">
+                <small>Precio</small>
+                <p>${producto.precio}</p>
+            </div>
+            <div class="carrito-subtotal-producto">
+                <small>Subtotal</small>
+                <p>${producto.precio * producto.cantidad}</p>
+            </div>
+            <button class="carrito-eliminar-producto" id=${producto.id}><i class="fa-solid fa-trash"></i></button>
+    
+            `
+    
+        carritoProductos.append(div);
+        })
+            
+    }else {
+        carritoVacio.classList.remove("disabled");
+        carritoProductos.classList.add("disabled");
+        carritoAcciones.classList.add("disabled");
+        carritoVaciar.classList.add("disabled")
+        contenedorCompra.classList.add("disabled");
+    }
+
+    actualizarBotonesEliminar();
+    actualizarTotal();
+}
+
+cargarProductosCarrito ();
+
+
+function actualizarBotonesEliminar (){
+    botonesEliminar = document.querySelectorAll (".carrito-eliminar-producto");
+
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito)
+    })
+}
+
+function eliminarDelCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const index = productosEnCarrito.findIndex (producto => producto.id === idBoton )
+    
+    productosEnCarrito.splice(index, 1);
+    cargarProductosCarrito()
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+
+}
+
+carritoVaciar.addEventListener ("click", vaciarCarrito)
+
+function vaciarCarrito (){
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    cargarProductosCarrito();
+}
+
+function actualizarTotal (){
+    total.innerHTML = productosEnCarrito.reduce ((acc, producto) => acc + (producto.precio * producto.cantidad), 0)
+}
+
+botonComprar.addEventListener("click", comprarCarrito);
+
+function comprarCarrito (){
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+        carritoVacio.classList.add("disabled");
+        carritoProductos.classList.add("disabled");
+        carritoAcciones.classList.add("disabled");
+        carritoVaciar.classList.add("disabled");
+        contenedorCompra.classList.remove("disabled");
+}
--- js/productos.js
+++ js/productos.js
@@ -1,26 +1,27 @@
 const contenedor = document.querySelector("#productos");
-const btnFiltro = document.querySelectorAll("input[type=checkbox]");
+const botonesCategorias = document.querySelectorAll(".form-check-label");
 const btnFiltrar = document.querySelector ("#btnfiltrar");
-const search = document.querySelector ("#search");
-const filtroTop= document.querySelector ("#inlineCheckbox1")
+let botonesAgregar = document.querySelectorAll (".btn");
+const numero = document.querySelector (".carrito-numero")
+
 
 const productos = [
-    { id: 1, nombre: "top MEXICO", precio: 5800, img: "top1.png", valor: "top"},
-    { id: 2, nombre: "top FRANCIA", precio: 5800, img: "top2.png" },
-    { id: 3, nombre: "top JAPON", precio: 5800, img: "top3.png" },
-    { id: 4, nombre: "top CONGO", precio: 5800, img: "top4.png" },
-    { id: 5, nombre: "top AUSTRALIA", precio: 5800, img: "top5.png" },
-    { id: 6, nombre: "top PERU", precio: 5800, img: "top6.png" },
-    { id: 7, nombre: "canillita CANADA", precio: 3500, img: "canillita1.png.jpeg" },
-    { id: 8, nombre: "canillita ESPAÑA", precio: 3500, img: "canillita2.png.jpeg" },
-    { id: 9, nombre: "canillita FILIPINAS", precio: 3500, img: "canillita3.png.png" },
-    { id: 10, nombre: "canillita TONGA", precio: 3500, img: "canillita4.png" },
-    { id: 11, nombre: "canillita NIGERIA", precio: 3500, img: "canillita5.png" },
-    { id: 12, nombre: "canillita BRASIL", precio: 3500, img: "canillita6.png.jpeg" },
-    { id: 13, nombre: "calza GUATEMALA", precio: 7200, img: "calza1.png" },
-    { id: 14, nombre: "calza ITALIA", precio: 7200, img: "calza2.png" },
-    { id: 15, nombre: "calza CHINA", precio: 7200, img: "calza3.png" },
-    { id: 16, nombre: "calza ARGENTINA", precio: 7200, img: "calza4.png" },
+    { id: "1", nombre: "top MEXICO", precio: 5800, img: "top1.png", valor: "top", categoria:{nombre: "tops", id: "top"}},
+    { id: "2", nombre: "top FRANCIA", precio: 5800, img: "top2.png", categoria:{nombre: "tops", id: "top"} },
+    { id: "3", nombre: "top JAPON", precio: 5800, img: "top3.png", categoria:{nombre: "tops", id: "top"} },
+    { id: "4", nombre: "top CONGO", precio: 5800, img: "top4.png", categoria:{nombre: "tops", id: "top"} },
+    { id: "5", nombre: "top AUSTRALIA", precio: 5800, img: "top5.png", categoria:{nombre: "tops", id: "top"} },
+    { id: "6", nombre: "top PERU", precio: 5800, img: "top6.png", categoria:{nombre: "tops", id: "top"} },
+    { id: "7", nombre: "canillita CANADA", precio: 3500, img: "canillita1.png.jpeg", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "8", nombre: "canillita ESPAÑA", precio: 3500, img: "canillita2.png.jpeg", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "9", nombre: "canillita FILIPINAS", precio: 3500, img: "canillita3.png.png", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "10", nombre: "canillita TONGA", precio: 3500, img: "canillita4.png", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "11", nombre: "canillita NIGERIA", precio: 3500, img: "canillita5.png", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "12", nombre: "canillita BRASIL", precio: 3500, img: "canillita6.png.jpeg", categoria:{nombre: "canillitas", id: "canillita"} },
+    { id: "13", nombre: "calza GUATEMALA", precio: 7200, img: "calza1.png", categoria:{nombre: "calzas", id: "calza"} },
+    { id: "14", nombre: "calza ITALIA", precio: 7200, img: "calza2.png", categoria:{nombre: "calzas", id: "calza"} },
+    { id: "15", nombre: "calza CHINA", precio: 7200, img: "calza3.png", categoria:{nombre: "calzas", id: "calza"} },
+    { id: "16", nombre: "calza ARGENTINA", precio: 7200, img: "calza4.png", categoria:{nombre: "calzas", id: "calza"} },
 ]
 
 //Crear HTML
@@ -34,78 +35,73 @@ function crearHtml(array) {
             `<div class="card galeria wow animate__animated animate__fadeInUp" style="width: 15rem; height: 23rem;">
                 <a href="" hover class="fotoprod"><img src="../assets/imagenes/${img}" class="card-img-top" alt="Top rosa con flores"/></a>
                 <div class="card-body">
-                    <a href="">
                         <h5 class="card-title">${nombre}</h5>
                         <p class="card-text">$ ${precio}</p>
-                        <p class="btn" id="${id}">Comprar <i class="fa-solid fa-bag-shopping"></i></p>
-                    </a>
+                        <button class="btn" id="${id}">Agregar <i class="fa-solid fa-bag-shopping"></i></button>
                 </div>
             </div>`
         contenedor.innerHTML += html;
     }
+    actualizarBotones()
 }
 
 crearHtml(productos);
 
-//Funciones de filtrado
-
-function filtrarProductos(filtro) {
-    let filtrado = productos.filter((el) => {
-        return el.nombre.includes(filtro);
-    });
-    return filtrado;
-}
-
-btnFiltrar.onclick = (e)=>{
-    e.preventDefault();
-    let filtro= filtrarProductos (search.value)
-    crearHtml(filtro)
-}
-
-//Carrito
-
-/* const carrito= [];
-
-function cargarProductos(arr, producto){
-    carrito.push(prompt("ingresa tu compra"));
-}
-
-cargarProductos()
-
-console.log(carrito); */
-
-/* function filtrarProductos(top) {
-    let filtrado = productos.filter((el) => {
-        return el.valor.includes(top);
-    });
-    return filtrado;
-} */
-
-/* let filtradoTop = productos.filter((el) => {
-    return el.value.includes(top);
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        if (e.currentTarget.id != "todo") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            crearHtml(productosBoton);
+        } else {
+            crearHtml(productos);
+        }
+    })
 });
-console.log(filtradoTop);
- */
 
-/* function filtrarProductos(top) {
-if (filtroTop.checked) {
-    let filtrado = productos.filter((el) => {
-        return el.nombre.includes(top);
-    });
-    return filtrado;
-}}
 
-btnFiltrar.onclick = (e)=>{
-    e.preventDefault();
-    let filtro= filtrarProductos (top)
-    crearHtml(filtro)
+
+function actualizarBotones (){
+    botonesAgregar = document.querySelectorAll (".btn");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito)
+    })
 }
 
-console.log(filtrarProductos(top));
+let productosEnCarrito;
 
-let checked;
-function filtrarProcutos (){
-if (filtroTop===checked) {
-        return filtro
+const productosEnCarritoLS = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+if(productosEnCarritoLS){
+    productosEnCarrito = productosEnCarritoLS;
+    actualizarNumero();
+}else{
+    productosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find (producto => producto.id === idBoton);
+
+    if (productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    }else {
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado)
     }
-} */
+    console.log(productosEnCarrito);
+
+    actualizarNumero()
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
+
+
+function actualizarNumero (){
+    let nuevoNumero = productosEnCarrito.reduce ((acc, producto) => acc + producto.cantidad, 0);
+    numero.innerHTML = nuevoNumero
+}
